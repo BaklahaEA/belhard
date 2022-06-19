1804eb6013a36b97c6c83c114ef4eb926e01bf5c
BaklahaEA <eugenebaklaga@gmail.com>
2022-06-19 23:30:09 +0300

    initial commit

---
+++ homeword #2 Lesson 3.py
@@ -0,0 +1,20 @@
+print(round(456.735785, 3))
+
+a = 384233
+b = 127399
+c = 234333
+d =  a * b / c
+print (d)
+
+v = a + b + c /33
+print (v)
+
+
+
+#самое задание
+a = int(input(3213.333))
+b = int(input(2323.333))
+c = int(input(232323.442))
+z = (a+b+c)/3
+print(z, 3)
+
